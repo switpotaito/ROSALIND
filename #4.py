2916b24b48a7ec162377def76da8074848430bc3
switpotaito <115626427+switpotaito@users.noreply.github.com>
2026-02-04 14:56:06 +0900

    Create #4.py

---
+++ #4.py
@@ -0,0 +1,12 @@
+#4. Rabbits and Recurrence Relations
+# Given: Positive integers n≤40 and k≤5
+# Return: The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k
+# rabbit pairs (instead of only 1 pair)
+
+def fibo(n,k):
+    if n <= 1:
+        return n
+    else:
+        return fibo(n-1,k) + k*fibo(n-2,k)
+
+  
