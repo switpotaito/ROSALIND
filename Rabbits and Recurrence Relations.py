03c83e9a6163920f1e2f66cc0724915afff74868
switpotaito <115626427+switpotaito@users.noreply.github.com>
2026-02-05 09:49:30 +0900

    Update and rename #4.py to Rabbits and Recurrence Relations.py

--- Rabbits and Recurrence Relations.py
+++ Rabbits and Recurrence Relations.py
@@ -1,4 +1,4 @@
-#4. Rabbits and Recurrence Relations
+#Rabbits and Recurrence Relations
 # Given: Positive integers n≤40 and k≤5
 # Return: The total number of rabbit pairs that will be present after n months, if we begin with 1 pair and in each generation, every pair of reproduction-age rabbits produces a litter of k
 # rabbit pairs (instead of only 1 pair)
