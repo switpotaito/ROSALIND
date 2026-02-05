7bbf38e363ec69e5053c05ea1c3b8b11a147273f
switpotaito <115626427+switpotaito@users.noreply.github.com>
2026-02-05 12:45:34 +0900

    Create Translating RNA into Protein.py

---
+++ Translating RNA into Protein.py
@@ -0,0 +1,39 @@
+#Translating RNA into Protein
+with open("rosalind_prot.txt", "r") as f:
+  seq = f.read()
+
+result = []
+for i in range(0, len(seq), 3):
+    result.append(seq[i:i+3])
+
+codon_table = {
+    "UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+    "UAU": "Y", "UAC": "Y", "UAA": "*", "UAG": "*",
+    "UGU": "C", "UGC": "C", "UGA": "*", "UGG": "W",
+    "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+    "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+    "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"
+}
+
+def translate(codon):
+    return codon_table.get(codon)
+
+result_codon = ""
+
+for codon in result:
+    if type(translate(codon)) == str:
+      result_codon = result_codon + translate(codon)
+    elif translate(codon) == "*":
+            break
+
+print(result_codon)
